6bc8ccade0dcc1fa7c3dcaafe3efff320ac7d753
Rushil <rushil.ramautar@gmail.com>
2023-01-23 19:05:18 -0300

    Added comparison to previous date visitors

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Member;
+use App\Models\Payment;
+use App\Models\Visit;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +27,24 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $activeMembers = Member::where('active', 1)->count();
+        $newMembersThisMonth = Member::whereMonth('created_at', Carbon::now()->month)->count();
+        $visitsYesterday = Visit::whereDate('time_of_arrival', Carbon::yesterday()->toDateString())->count();
+        $visitsToday = Visit::whereDate('time_of_arrival', Carbon::today()->toDateString())->count();
+        $revenueThisMonth = Payment::whereMonth('created_at', Carbon::today()->month)->sum('balance');
+
+        if ($visitsToday != 0 && $visitsYesterday != 0) {
+            $visitorDifference = ((number_format(($visitsToday / $visitsYesterday), 2)) * 100) - 100;
+        } else {
+            $visitorDifference = 0;
+        }
+
+        return view('home', [
+            'activeMembers' => $activeMembers,
+            'newMembersThisMonth' => $newMembersThisMonth,
+            'visitsToday' => $visitsToday,
+            'visitorDifference' => $visitorDifference,
+            'revenueThisMonth' => $revenueThisMonth,
+        ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,13 +9,13 @@
                     <div class="card radius-10 bg-gradient-deepblue">
                         <div class="card-body">
                         <div class="d-flex align-items-center">
-                            <h5 class="mb-0 text-white">50</h5>
+                            <h5 class="mb-0 text-white">{{ $activeMembers }}</h5>
                             <div class="ms-auto">
                                 <i class="bi bi-person-check fs-3 text-white"></i>
                             </div>
                         </div>
                         <div class="progress my-3 bg-light-transparent" style="height:3px;">
-                            <div class="progress-bar bg-white" role="progressbar" style="width: 55%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                            <div class="progress-bar bg-white" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
                         </div>
                         <div class="d-flex align-items-center text-white">
                             <p class="mb-0">Active Members</p>
@@ -28,17 +28,19 @@
                     <div class="card radius-10 bg-gradient-orange">
                         <div class="card-body">
                         <div class="d-flex align-items-center">
-                            <h5 class="mb-0 text-white">5</h5>
+                            <h5 class="mb-0 text-white">{{ $newMembersThisMonth }}</h5>
                             <div class="ms-auto">
                                 <i class="bi bi-person-add fs-3 text-white"></i>
                             </div>
                         </div>
                         <div class="progress my-3 bg-light-transparent" style="height:3px;">
-                            <div class="progress-bar bg-white" role="progressbar" style="width: 55%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                            <div class="progress-bar bg-white" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
                         </div>
                         <div class="d-flex align-items-center text-white">
                             <p class="mb-0">New Members</p>
-                            {{-- <p class="mb-0 ms-auto">+4.2%<span><i class="bx bx-up-arrow-alt"></i></span></p> --}}
+                            <p class="mb-0 ms-auto">
+                                {{ Str::ucfirst(\Carbon\Carbon::today()->locale('nl')->monthName) }}
+                            </p>
                         </div>
                     </div>
                     </div>
@@ -47,17 +49,17 @@
                     <div class="card radius-10 bg-gradient-ohhappiness">
                         <div class="card-body">
                         <div class="d-flex align-items-center">
-                            <h5 class="mb-0 text-white">40</h5>
+                            <h5 class="mb-0 text-white">{{ $visitsToday }}</h5>
                             <div class="ms-auto">
                                 <i class="bi bi-door-open fs-3 text-white"></i>
                             </div>
                         </div>
                         <div class="progress my-3 bg-light-transparent" style="height:3px;">
-                            <div class="progress-bar bg-white" role="progressbar" style="width: 55%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                            <div class="progress-bar bg-white" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
                         </div>
                         <div class="d-flex align-items-center text-white">
                             <p class="mb-0">Visits Today</p>
-                            {{-- <p class="mb-0 ms-auto">+4.2%<span><i class="bx bx-up-arrow-alt"></i></span></p> --}}
+                            <p class="mb-0 ms-auto">{{ $visitorDifference }}%<span><i class="bx bx-up-arrow-alt"></i></span></p>
                         </div>
                     </div>
                     </div>
@@ -66,17 +68,17 @@
                     <div class="card radius-10 bg-gradient-ibiza">
                         <div class="card-body">
                         <div class="d-flex align-items-center">
-                            <h5 class="mb-0 text-white">SRD 10,000</h5>
+                            <h5 class="mb-0 text-white">SRD {{ $revenueThisMonth }}</h5>
                             <div class="ms-auto">
                                 <i class="bi bi-currency-dollar fs-3 text-white"></i>
                             </div>
                         </div>
                         <div class="progress my-3 bg-light-transparent" style="height:3px;">
-                            <div class="progress-bar bg-white" role="progressbar" style="width: 55%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                            <div class="progress-bar bg-white" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
                         </div>
                         <div class="d-flex align-items-center text-white">
                             <p class="mb-0">Total Revenue</p>
-                            {{-- <p class="mb-0 ms-auto">+4.2%<span><i class="bx bx-up-arrow-alt"></i></span></p> --}}
+                            <p class="mb-0 ms-auto">{{ Str::ucfirst(\Carbon\Carbon::today()->locale('nl')->monthName) }}</p>
                         </div>
                     </div>
                     </div>
